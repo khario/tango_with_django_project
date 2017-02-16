1c9e6460c481376968c2d4a0313bd4c236444df6
khario <khari.outram@gmail.com>
2017-02-16 10:24:39 +0000

    End of chapter 10

--- rango/urls.py
+++ rango/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
     url(r'^logout/$', views.user_logout, name='logout'),
-  
+
 
 ]
--- rango/views.py
+++ rango/views.py
@@ -7,12 +7,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 #from django.http import HttpResponse
 
 #import the Category model
 from rango.models import Category
 from rango.models import Page
+
+
+
 
 def index(request):
     # Query the database for a list of ALL categories currently store.
@@ -21,25 +25,80 @@ def index(request):
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
 
+    request.session.set_test_cookie()
     context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('views')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+    # Call function to handle the cookies
+    visitor_cookie_handler(request)
+
+    context_dict['visits'] = request.session['visits']
+
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    #return render(request, 'rango/index.html', context_dict)
 
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] =  last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
 
 #def about(request):
 #    return HttpResponse("Rango says here is the about page." '<br/> <a href="/rango/">Click here to go to the home page</a>')
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    # Call function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/about.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
+    #return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
